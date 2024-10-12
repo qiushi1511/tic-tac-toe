8c5ad3139c2429e4ced30fe58b177520cd9977b6
alex <qiushi1511@gmail.com>
2024-10-12 19:37:27 +0800

    Reduce test coverage 2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import Heading from "./Heading";
 import Game from "./Game.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <Heading />
     <Game />
   </StrictMode>
 );
